0ca663cdbf3361a490bdda9afda225e8b7b8a67e
ken2201 <ndayishimiyeken22@gmail.com>
2024-09-02 10:07:07 +0200

    refactor: Simplify factorial function and improve error handling

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -3,27 +3,12 @@ import sys
 
 
 def factorial(n):
-    if n < 0:
-        return "Factorial is not defined for negative numbers"
-    elif n == 0:
-        return 1
-    else:
-        result = 1
-        while n > 1:
-            result *= n
-            n -= 1
-        return result
+    result = 1
+    while n > 1:
+        result *= n
+        n = n - 1
+    return result
 
 
-if len(sys.argv) != 2:
-    print("Usage: python3 script.py <number>")
-    sys.exit(1)
-
-try:
-    number = int(sys.argv[1])
-except ValueError:
-    print("Please provide a valid integer")
-    sys.exit(1)
-
-result = factorial(number)
-print(result)
+f = factorial(int(sys.argv[1]))
+print(f)
